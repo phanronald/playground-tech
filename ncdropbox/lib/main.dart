905444de376a53daacc444aa9fd16d34f8fe3467
Ronald Phan <phanronald@gmail.com>
2021-10-09 15:32:51 -0700

    added circle avatar, divider and sizedbox

--- ncdropbox/lib/main.dart
+++ ncdropbox/lib/main.dart
@@ -10,42 +10,85 @@ class MyHomePage extends StatelessWidget {
     return MaterialApp(
       title: 'Welcome to Flutter Test',
       home: Scaffold(
+        backgroundColor: Colors.grey[900],
         appBar: AppBar(
           title: const Text('Welcome to Irys Tests'),
           centerTitle: true,
-          backgroundColor: Colors.red[600],
+          backgroundColor: Colors.grey[850],
+          elevation: 0.0,
         ),
-        body: Row(
-          children: [
-            Expanded(
-              flex: 3,
-              child: Image.asset('assets/baby-irys-pokemon.jpg'),
-            ),
-            Expanded(
-              flex: 1,
-              child: Container(
-                padding: const EdgeInsets.all(30.0),
-                color: Colors.cyan,
-                child: const Text('1')
+        body: Padding(
+          padding: const EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Center(
+                child: CircleAvatar(
+                  backgroundImage: AssetImage('assets/baby-irys.jpg'),
+                  radius: 40.0,
+                ),
               ),
-            ),
-            Expanded(
-              flex: 1,
-              child: Container(
-                  padding: const EdgeInsets.all(30.0),
-                  color: Colors.pinkAccent,
-                  child: const Text('2')
+              Divider(
+                height: 60.0,
+                color: Colors.grey[800],
               ),
-            ),
-            Expanded(
-              flex: 1,
-              child: Container(
-                  padding: const EdgeInsets.all(30.0),
-                  color: Colors.amber,
-                  child: const Text('3')
+              const Text(
+                'NAME',
+                style: TextStyle(
+                  color: Colors.grey,
+                  letterSpacing: 2.0,
+                )
               ),
-            ),
-          ]
+              const SizedBox(height: 10.0),// add invisible div for height instead of padding
+              Text(
+                  'Irys',
+                  style: TextStyle(
+                    color: Colors.amberAccent[200],
+                    letterSpacing: 2.0,
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.bold,
+                  )
+              ),
+              const SizedBox(height: 30.0),// add invisible div for height instead of padding
+              const Text(
+                  'will battle against',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    letterSpacing: 2.0,
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.bold,
+                  )
+              ),
+              const SizedBox(height: 10.0),
+              const Text(
+                  'Calli',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    letterSpacing: 2.0,
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.bold,
+                  )
+              ),
+              const SizedBox(height: 30.0),
+              Row(
+                children: [
+                  Icon(
+                    Icons.email,
+                    color: Colors.grey[400]
+                  ),
+                  const SizedBox(width: 10.0),
+                  Text(
+                    'irys@calli.net',
+                    style: TextStyle(
+                      color: Colors.grey[400],
+                      fontSize: 18.0,
+                      letterSpacing: 1.0,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
