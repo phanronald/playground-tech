a28c82437663a49b9d04e980d4f651b985ea9533
Ronald Phan <phanronald@gmail.com>
2021-10-09 17:33:43 -0700

    moved to a widget function and have listview use it

--- ncdropbox/lib/main.dart
+++ ncdropbox/lib/main.dart
@@ -35,11 +35,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 padding: const EdgeInsets.all(8),
                 itemCount: hololivemem.length,
                 itemBuilder: (BuildContext context, int index) {
-                  return Container(
-                    height: 50,
-                    color: Colors.amberAccent,
-                    child: Center(child: Text('Entry ${hololivemem[index].name}')),
-                  );
+                  return hololiveCard(hololivemem[index]);
                 }
             ),
         ),
@@ -52,5 +48,35 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
   }
+
+ Widget hololiveCard(Hololive hololive) {
+   return Card(
+     margin: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
+     child: Padding(
+       padding: const EdgeInsets.all(12.0),
+       child: Column(
+           crossAxisAlignment: CrossAxisAlignment.stretch,
+           children: [
+             Text(
+                 hololive.name,
+                 style: TextStyle(
+                   fontSize: 18.0,
+                   color: Colors.grey[600],
+                 )
+             ),
+             const SizedBox(height: 6.0),
+             Text(
+                 hololive.description,
+                 style: TextStyle(
+                   fontSize: 14.0,
+                   color: Colors.grey[800],
+                 )
+             ),
+           ]
+       ),
+     ),
+   );
+ }
+
 }
 
